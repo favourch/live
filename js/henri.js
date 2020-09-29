787560aa2c115c0a0241534b4f1a403ee563c964
Liam Rice <liam@henristudios.co>
2020-09-29 11:25:55 +0100

    first upload (live website)

---
+++ js/henri.js
@@ -0,0 +1,9 @@
+/* Shrink navbar */
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+
+  }
+});
